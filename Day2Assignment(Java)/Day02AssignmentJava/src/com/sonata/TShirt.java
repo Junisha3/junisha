ed56fabf3a997bd197e6eebfb81a55078350b643
Divi. Junisha <divi.junisha21@gmail.com>
2021-12-04 21:35:19 +0530

    Day2Assignment Completed

---
+++ Day2Assignment(Java)/Day02AssignmentJava/src/com/sonata/TShirt.java
@@ -0,0 +1,57 @@
+package com.sonata;
+
+public class TShirt {
+		String color;
+		String material;
+		String design;
+		String Size;
+		
+		public TShirt(String co,String ma,String de,String si) {
+		this.color=co;
+		this.material=ma;
+		this.design=de;
+		this.Size=si;
+		
+		}
+		
+		public void Small() {
+			
+			System.out.println("This is Small size");
+			
+		}
+		
+		public void Large() {
+			System.out.println("This is Large size");
+			
+		}
+		public void ExtraLarge() {
+			System.out.println("This is ExtraLarge size");
+		}
+		
+		public void display() {
+			System.out.println("Color: "+color);
+			System.out.println();
+			System.out.println("Material: "+material);
+			System.out.println();
+			System.out.println("Design: "+design);
+			System.out.println();
+			System.out.println("Size: "+Size);
+			System.out.println();
+			
+		}
+		
+		public static void main(String[]args) {
+			TShirt t1=new TShirt("pink","cotton","Fullsleave","S");
+			System.out.println();
+			t1.display();
+			t1.Small();
+			TShirt t2=new TShirt("Blue","cotton","sleaveless","L");
+			System.out.println();
+			t2.display();
+			t2.Large();
+			TShirt t3=new TShirt("Black","cotton","Fullsleave","XL");
+			System.out.println();
+			t3.display();
+			t3.ExtraLarge();
+		}
+	}
